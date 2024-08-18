db136011d1f0690bcf06bad9fc21faf55da3774f
BarAmir12 <baramir12@gmail.com>
2024-08-18 18:06:41 +0300

    bar

--- pet-supplies-client/src/components/UpdateProductForm.js
+++ pet-supplies-client/src/components/UpdateProductForm.js
@@ -1,68 +1,73 @@
-import React, { useEffect, useState } from 'react';
-import '../css/AddProductsForm.css';
-import axios from 'axios';
-import { useNavigate, useParams } from 'react-router-dom';
+import React, { useEffect, useState } from 'react'
+import '../css/AddProductsForm.css'
+import axios from 'axios'
+import { useNavigate, useParams } from 'react-router-dom'
 
 const UpdateProductForm = () => {
   const [formData, setFormData] = useState({
     animalCategory: '',
-    productCategory: '',
+    category: '',
     name: '',
     description: '',
     price: '',
     brand: '',
     stock: '',
     imageUrl: '',
-  });
+  })
 
-  const [message, setMessage] = useState('');
-  const navigate = useNavigate();
-  const { id } = useParams(); // קבלת מזהה המוצר מקובץ הנתיב
+  const [message, setMessage] = useState('')
+  const navigate = useNavigate()
+  const { id } = useParams() // קבלת מזהה המוצר מקובץ הנתיב
 
   // Fetch product details for the initial load
   useEffect(() => {
     const fetchProductDetails = async () => {
       try {
-        const response = await axios.get(`http://localhost:5000/api/products/product/${id}`);
-        setFormData(response.data); // ממלא את המידע עם פרטי המוצר
+        const response = await axios.get(
+          `http://localhost:5000/api/products/product/${id}`
+        )
+        setFormData(response.data) // ממלא את המידע עם פרטי המוצר
       } catch (error) {
-        console.error('Error fetching product details:', error);
+        console.error('Error fetching product details:', error)
       }
-    };
+    }
 
-    fetchProductDetails();
-  }, [id]);
+    fetchProductDetails()
+  }, [id])
 
   const handleChange = (e) => {
-    const { name, value } = e.target;
+    const { name, value } = e.target
     setFormData({
       ...formData,
-      [name]: value
-    });
-  };
+      [name]: value,
+    })
+  }
 
   const handleSubmit = async (e) => {
-    e.preventDefault();
-    const confirmUpdate = window.confirm("Do you want to update this product?");
+    e.preventDefault()
+    const confirmUpdate = window.confirm('Do you want to update this product?')
     if (!confirmUpdate) {
-      setMessage("Product's update canceled.");
-      return;
+      setMessage("Product's update canceled.")
+      return
     }
 
     try {
-      const response = await axios.put(`http://localhost:5000/api/products/product/${id}`, formData); // שינוי ל- PUT
+      const response = await axios.put(
+        `http://localhost:5000/api/products/product/${id}`,
+        formData
+      ) // שינוי ל- PUT
       if (response.status === 200) {
-        setMessage('Product updated successfully!');
-        console.log('Product updated successfully:', response.data);
-        navigate('/manageproducts'); // ניווט לדף הבית או לדף אחר לאחר העדכון
+        setMessage('Product updated successfully!')
+        console.log('Product updated successfully:', response.data)
+        navigate('/manageproducts') // ניווט לדף הבית או לדף אחר לאחר העדכון
       } else {
-        setMessage('Failed to update product.');
+        setMessage('Failed to update product.')
       }
     } catch (error) {
-      console.error('Error updating product:', error);
-      setMessage('Error updating product.');
+      console.error('Error updating product:', error)
+      setMessage('Error updating product.')
     }
-  };
+  }
 
   return (
     <div className="add-product-form">
@@ -70,7 +75,12 @@ const UpdateProductForm = () => {
       <form onSubmit={handleSubmit}>
         <div className="form-group">
           <label>קטגוריית החיה:</label>
-          <select name="animalCategory" value={formData.animalCategory} onChange={handleChange} required>
+          <select
+            name="animalCategory"
+            value={formData.animalCategory}
+            onChange={handleChange}
+            required
+          >
             <option value="">בחר חיה</option>
             <option value="dog">כלב</option>
             <option value="cat">חתול</option>
@@ -82,7 +92,12 @@ const UpdateProductForm = () => {
         </div>
         <div className="form-group">
           <label>קטגוריית המוצר:</label>
-          <select name="productCategory" value={formData.productCategory} onChange={handleChange} required>
+          <select
+            name="category"
+            value={formData.category}
+            onChange={handleChange}
+            required
+          >
             <option value="">בחר קטגוריה</option>
             <option value="food">מזון</option>
             <option value="toy">צעצוע</option>
@@ -91,33 +106,69 @@ const UpdateProductForm = () => {
         </div>
         <div className="form-group">
           <label>שם המוצר:</label>
-          <input type="text" name="name" value={formData.name} onChange={handleChange} required />
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
         </div>
         <div className="form-group">
           <label>תיאור:</label>
-          <textarea name="description" value={formData.description} onChange={handleChange} required />
+          <textarea
+            name="description"
+            value={formData.description}
+            onChange={handleChange}
+            required
+          />
         </div>
         <div className="form-group">
           <label>מחיר:</label>
-          <input type="number" name="price" value={formData.price} onChange={handleChange} required />
+          <input
+            type="number"
+            name="price"
+            value={formData.price}
+            onChange={handleChange}
+            required
+          />
         </div>
         <div className="form-group">
           <label>מותג:</label>
-          <input type="text" name="brand" value={formData.brand} onChange={handleChange} required />
+          <input
+            type="text"
+            name="brand"
+            value={formData.brand}
+            onChange={handleChange}
+            required
+          />
         </div>
         <div className="form-group">
           <label>מלאי:</label>
-          <input type="number" name="stock" value={formData.stock} onChange={handleChange} required />
+          <input
+            type="number"
+            name="stock"
+            value={formData.stock}
+            onChange={handleChange}
+            required
+          />
         </div>
         <div className="form-group">
           <label htmlFor="imageUrl">Image URL:</label>
-          <input type="text" id="imageUrl" name="imageUrl" value={formData.imageUrl} onChange={handleChange} required />
+          <input
+            type="text"
+            id="imageUrl"
+            name="imageUrl"
+            value={formData.imageUrl}
+            onChange={handleChange}
+            required
+          />
         </div>
         <button type="submit">עדכן פריט</button>
       </form>
       {message && <p className="message">{message}</p>}
     </div>
-  );
-};
+  )
+}
 
-export default UpdateProductForm;
+export default UpdateProductForm
--- pet-supplies-client/src/pages/AddProductsForm.js
+++ pet-supplies-client/src/pages/AddProductsForm.js
@@ -1,70 +1,73 @@
-import React, { useState } from 'react';
-import '../css/AddProductsForm.css';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from 'react'
+import '../css/AddProductsForm.css'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 const AddProductForm = () => {
   const [formData, setFormData] = useState({
     animalCategory: '',
-    productCategory: '',
+    category: '',
     name: '',
     description: '',
     price: '',
     brand: '',
     stock: '',
     imageUrl: '',
-  });
+  })
 
-  const [message, setMessage] = useState('');
-  const navigate = useNavigate();
+  const [message, setMessage] = useState('')
+  const navigate = useNavigate()
 
   const handleChange = (e) => {
-    const { name, value } = e.target;
+    const { name, value } = e.target
     setFormData({
       ...formData,
-      [name]: value
-    });
-  };
+      [name]: value,
+    })
+  }
 
   const handleSubmit = async (e) => {
-    e.preventDefault();
+    e.preventDefault()
 
-    const confirmAdd = window.confirm("Do you want to add this product?");
+    const confirmAdd = window.confirm('Do you want to add this product?')
     if (!confirmAdd) {
-      setMessage("Product's addition canceled.");
-      return;
+      setMessage("Product's addition canceled.")
+      return
     }
 
     // Validate that imageUrl is a valid URL (optional)
     if (!isValidUrl(formData.imageUrl)) {
-      setMessage("Invalid image URL.");
-      return;
+      setMessage('Invalid image URL.')
+      return
     }
 
     try {
-      const response = await axios.post('http://localhost:5000/api/products/addproduct', formData);
+      const response = await axios.post(
+        'http://localhost:5000/api/products/addproduct',
+        formData
+      )
       if (response.status === 201) {
-        setMessage('Product added successfully!');
-        console.log('Product added successfully:', response.data);
-        navigate('/'); // Navigate to home page
+        setMessage('Product added successfully!')
+        console.log('Product added successfully:', response.data)
+        navigate('/') // Navigate to home page
       } else {
-        setMessage('Failed to add product.');
+        setMessage('Failed to add product.')
       }
     } catch (error) {
-      console.error('Error adding product:', error);
-      setMessage('Error adding product.');
+      console.error('Error adding product:', error)
+      setMessage('Error adding product.')
     }
-  };
+  }
 
   // Helper function to validate URL
-  const isValidUrl = string => {
+  const isValidUrl = (string) => {
     try {
-      new URL(string); // Trying to create a URL object will throw an error if invalid
-      return true;
+      new URL(string) // Trying to create a URL object will throw an error if invalid
+      return true
     } catch (_) {
-      return false; // Return false if invalid
+      return false // Return false if invalid
     }
-  };
+  }
 
   return (
     <div className="add-product-form">
@@ -72,7 +75,12 @@ const AddProductForm = () => {
       <form onSubmit={handleSubmit}>
         <div className="form-group">
           <label>קטגוריית החיה:</label>
-          <select name="animalCategory" value={formData.animalCategory} onChange={handleChange} required>
+          <select
+            name="animalCategory"
+            value={formData.animalCategory}
+            onChange={handleChange}
+            required
+          >
             <option value="">בחר חיה</option>
             <option value="dog">כלב</option>
             <option value="cat">חתול</option>
@@ -80,12 +88,16 @@ const AddProductForm = () => {
             <option value="fish">דג נוי</option>
             <option value="reptile">זוחל</option>
             <option value="rotend">מכרסם</option>
-
           </select>
         </div>
         <div className="form-group">
           <label>קטגוריית המוצר:</label>
-          <select name="productCategory" value={formData.productCategory} onChange={handleChange} required>
+          <select
+            name="category"
+            value={formData.category}
+            onChange={handleChange}
+            required
+          >
             <option value="">בחר קטגוריה</option>
             <option value="food">מזון</option>
             <option value="toy">צעצוע</option>
@@ -94,34 +106,70 @@ const AddProductForm = () => {
         </div>
         <div className="form-group">
           <label>שם המוצר:</label>
-          <input type="text" name="name" value={formData.name} onChange={handleChange} required />
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
         </div>
         <div className="form-group">
           <label>תיאור:</label>
-          <textarea name="description" value={formData.description} onChange={handleChange} required />
+          <textarea
+            name="description"
+            value={formData.description}
+            onChange={handleChange}
+            required
+          />
         </div>
         <div className="form-group">
           <label>מחיר:</label>
-          <input type="number" name="price" value={formData.price} onChange={handleChange} required />
+          <input
+            type="number"
+            name="price"
+            value={formData.price}
+            onChange={handleChange}
+            required
+          />
         </div>
         <div className="form-group">
           <label>מותג:</label>
-          <input type="text" name="brand" value={formData.brand} onChange={handleChange} required />
+          <input
+            type="text"
+            name="brand"
+            value={formData.brand}
+            onChange={handleChange}
+            required
+          />
         </div>
         <div className="form-group">
           <label>מלאי:</label>
-          <input type="number" name="stock" value={formData.stock} onChange={handleChange} required />
+          <input
+            type="number"
+            name="stock"
+            value={formData.stock}
+            onChange={handleChange}
+            required
+          />
         </div>
         <div className="form-group">
           <label htmlFor="imageUrl">Image URL:</label>
-          <input type="text" id="imageUrl" name="imageUrl" placeholder="הכנס כתובת קישור לתמונה" value={formData.imageUrl} onChange={handleChange} required />
+          <input
+            type="text"
+            id="imageUrl"
+            name="imageUrl"
+            placeholder="הכנס כתובת קישור לתמונה"
+            value={formData.imageUrl}
+            onChange={handleChange}
+            required
+          />
         </div>
         <button type="submit">הוסף פריט</button>
       </form>
       {message && <p className="message">{message}</p>}
     </div>
-  );
-};
+  )
+}
 
-export default AddProductForm;
-
+export default AddProductForm
