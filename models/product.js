825db6b102023adae081e641becbe49d0ee13451
BarAmir12 <baramir12@gmail.com>
2024-08-18 19:39:37 +0300

    bar

--- models/product.js
+++ models/product.js
@@ -1,19 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 const productSchema = new mongoose.Schema({
-    animalCategory: { type: String, required: true }, // סוג החיה (למשל: 'dog', 'cat')
-    productCategory: { type: String, required: true },
-    name: { type: String, required: true },
-    description: { type: String, required: true },
-    price: { type: Number, required: true },
-    brand: { type: String, required: true },
-    stock: { type: Number, required: true },
-    createdAt: { type: Date, default: Date.now }, // תאריך יצירה
-    imageUrl: { type: String, required: true }
-});
+  animalCategory: { type: String, required: true }, // סוג החיה (למשל: 'dog', 'cat')
+  category: { type: String, required: true },
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  brand: { type: String, required: true },
+  stock: { type: Number, required: true },
+  createdAt: { type: Date, default: Date.now }, // תאריך יצירה
+  imageUrl: { type: String, required: true },
+})
 
+const Product = mongoose.model('Product', productSchema)
 
-
-const Product = mongoose.model('Product', productSchema);
-
-module.exports = Product;   
+module.exports = Product
